80f55b264ade30ebd17325ec13a29ac3e91fb837
MgKyaw <mgkyaw20112@gmail.com>
2025-01-27 22:56:48 +0700

    feat: Show animal IDs

--- Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/Program.cs
+++ Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -287,7 +287,15 @@ do
 
         case "3":
             // Ensure animal ages and physical descriptions are complete
-            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    System.Console.WriteLine(ourAnimals[i, 0]);
+                }
+            }
+            
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
