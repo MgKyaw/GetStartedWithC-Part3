472e31d95d1e1b78ddd88e2831942462fa196b95
MgKyaw <mgkyaw20112@gmail.com>
2025-01-28 22:38:50 +0700

    feat: Add successful message

--- Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/Program.cs
+++ Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -335,6 +335,7 @@ do
                 }
             }
 
+            Console.WriteLine("Age and physical description fields are complete for all of our friends.");
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
